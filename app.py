2301725d3cee06b4bb25240b56c059f3b2c92b46
aloise402 <Aloise402@gmail.com>
2025-09-17 15:49:49 -0300

    Update app.py

--- app.py
+++ app.py
@@ -27,4 +27,5 @@ def api_full():
         return jsonify({"error": f"Failed to read cached data: {e}"}), 500
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    port = int(os.environ.get("PORT", 8080))
+    app.run(host="0.0.0.0", port=port, debug=False)
